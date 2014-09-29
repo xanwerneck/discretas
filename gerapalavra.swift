30333889c751075ae21a257a38ffe94136bd0c4b
Patrick Sava <patrick.sava@gmail.com>
2014-09-29 14:34:48 -0300

    Gera Palavras Swift
    acerto no comentário.

--- gerapalavra.swift
+++ gerapalavra.swift
@@ -5,8 +5,10 @@
     var alfabeto:[String] = [ "a", "b", "c" ]
 	var palavra:[String] = [ "", "", ""]
 	
-	// func <nomedafuncao>(p1:Tipo withOutroParametro p2:Tipo 
-							outroParametro p3:Tipo atecansar pN:Tipo) -> TipoRetorno{}
+	/* 
+	 * func <nomedafuncao>(p1:Tipo withOutroParametro p2:Tipo 
+	 *	outroParametro p3:Tipo atecansar pN:Tipo) -> TipoRetorno{}
+	 */
 	func executa(i:Int){
 	    if(i == 0){
 	        var combination = palavra[0] + palavra[1] + palavra[2]
