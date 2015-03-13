c7f6132a81e64e17ad3596e5c8f2bab1385f9dc3
Hugo Roque <hugolnx@gmail.com>
2015-03-12 21:13:42 -0300

    AA: Intervalo com maior soma em O(n2)

--- maior_soma.rb
+++ maior_soma.rb
@@ -1,10 +1,9 @@
 def bigger_sum(a)
   somas = Hash.new(0)
+  n = a.size
   a.each_with_index do |_, i|
-    a.each_with_index do |_, j|
-      for k in i..j
-        somas[i..j] += a[k]
-      end
+    for j in i...n
+      somas[i..j] += somas[i..(j-1)] + a[j]
     end
   end
   maior = 0
@@ -21,7 +20,8 @@ end
 TEST_CASES = [
   [[1, 2, 3], [0,2]],
   [[-1, 2, 3], [1,2]],
-  [[1, -2, 3], [2,2]]
+  [[1, -2, 3], [2,2]],
+  [[1, -2, 100, -91, 40, 50], [2,2]]
 ]
 
 failed = TEST_CASES.find{|(array, expected)| bigger_sum(array) != expected}
