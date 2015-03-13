b75c33a94a5d6bf84531f0812af16689101fe80b
Hugo Roque <hugolnx@gmail.com>
2015-03-12 21:07:53 -0300

    AA: Intervalo com maior soma em O(n3)

---
+++ maior_soma.rb
@@ -0,0 +1,34 @@
+def bigger_sum(a)
+  somas = Hash.new(0)
+  a.each_with_index do |_, i|
+    a.each_with_index do |_, j|
+      for k in i..j
+        somas[i..j] += a[k]
+      end
+    end
+  end
+  maior = 0
+  range_maior = nil
+  somas.each do |range, soma|
+    if soma > maior
+      maior = soma
+      range_maior = range
+    end
+  end
+  [range_maior.begin, range_maior.end]
+end
+
+TEST_CASES = [
+  [[1, 2, 3], [0,2]],
+  [[-1, 2, 3], [1,2]],
+  [[1, -2, 3], [2,2]]
+]
+
+failed = TEST_CASES.find{|(array, expected)| bigger_sum(array) != expected}
+if failed.nil?
+  puts "Os testes passaram"
+else
+  array = failed[0]
+  expected = failed[1]
+  puts "O caso #{array.inspect} falhou, retornou #{bigger_sum(array).inspect} mas deveria ser #{expected.inspect}"
+end
